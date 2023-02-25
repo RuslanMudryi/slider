0c4927eb2dcabc1135a318baa8a37580a551307f
RuslanMudruj <13072003.rm@gmail.com>
2023-02-25 12:00:28 +0200

    delete garbage in the code

--- js/script.js
+++ js/script.js
@@ -12,11 +12,7 @@ const sliderSettings = {
 
 }
 
-fetch('https://item-value5.appspot.com/ItemValue?i=1&id=MisterRuslanchik&app=730&ts=1677257605&key=6ecd6dc2cf96ce108f3bc4d90015208689da5e23')
-.then((response)=> response.data)
-.then((data)=>{
-        console.log(data);
-})
+
 
 
 
